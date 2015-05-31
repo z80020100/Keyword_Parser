5de631799f7dfa82fd71a7a052346ad3f0cca920
z80020100 <z800201002005@gmail.com>
2015-05-31 12:28:58 +0800

    Finish

--- Keyword_Parser/main.cpp
+++ Keyword_Parser/main.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <algorithm>
 #include <vector>
+#include <dirent.h>
 
 using namespace std;
 
@@ -19,8 +20,8 @@ bool char_reco(char c)
 
 int main()
 {
-	fstream doc_file, index_file;
-	string doc_path, index_path;
+	fstream doc_file, list_file;
+	string doc_path, list_path;
 	char buf[1024];
 	int start, end, length;
 
@@ -29,70 +30,84 @@ int main()
 	vector<string> v;
 	vector<string>::iterator iter;
 	
-	file_name = "2";
+	DIR *dp;
+	struct dirent *ep;
+	doc_path = "./Doc/";
 
-	doc_path = "./Doc/" + file_name;
-	doc_file.open(doc_path, ios::in);
-	if (!doc_file)
-		cerr << "Error: open " << doc_path << "  failed..." << endl;
-	else
+	dp = opendir(doc_path.c_str()); // for each file f, create a list f_bar of unique keyword
+	if (dp != NULL)
 	{
-		memset(buf, 0, sizeof(buf));
-		while (doc_file.getline(buf, sizeof(buf)))
+		readdir(dp); // .
+		readdir(dp); // ..
+		while (ep = readdir(dp))
 		{
-			//cout << buf << endl;
-
-			for (int i = 0; i < sizeof(buf); i++)
+			v.clear();
+			printf("Processing file: %s\n", ep->d_name);
+			file_name.assign(ep->d_name);
+			doc_path = "./Doc/" + file_name;
+			doc_file.open(doc_path, ios::in);
+			if (!doc_file)
+				cerr << "Error: open " << doc_path << "  failed..." << endl;
+			else
 			{
-				if (char_reco(buf[i]))
+				memset(buf, 0, sizeof(buf));
+				while (doc_file.getline(buf, sizeof(buf)))
 				{
-					//cout << buf[i] << endl;
-					start = i;
-					for (int j = i + 1; j < sizeof(buf); j++)
+					//cout << buf << endl;
+
+					for (int i = 0; i < sizeof(buf); i++)
 					{
-						if (!char_reco(buf[j]))
+						if (char_reco(buf[i]))
 						{
-							end = j;
-							length = end - start;
-							if (length > 0)
+							//cout << buf[i] << endl;
+							start = i;
+							for (int j = i + 1; j < sizeof(buf); j++)
 							{
-								keyword.assign(buf, start, length);
-								//cout << keyword << endl;
-								iter = find(v.begin(), v.end(), keyword);
-								if (iter == v.end())
+								if (!char_reco(buf[j]))
 								{
-									v.push_back(keyword);
+									end = j;
+									length = end - start;
+									if (length > 0)
+									{
+										keyword.assign(buf, start, length);
+										//cout << keyword << endl;
+										iter = find(v.begin(), v.end(), keyword);
+										if (iter == v.end())
+										{
+											v.push_back(keyword);
+										}
+										i = j;
+										break;
+									}
 								}
-								i = j;
-								break;
 							}
 						}
 					}
+					memset(buf, 0, sizeof(buf));
 				}
+
+				sort(v.begin(), v.end());
+
+				list_path = "./List/" + file_name + ".list";
+				list_file.open(list_path, ios::out);
+				if (!list_file)
+					cerr << "Error: open " << list_path << "  failed..." << endl;
+				else
+				{
+
+					for (int i = 0; i < v.size(); i++)
+					{
+						//cout << v[i] << endl;
+						list_file << v[i] << '\n';
+					}
+					doc_file.close();
+				}
+				list_file.close();
 			}
-			memset(buf, 0, sizeof(buf));
+
+			cout << "Parse " << v.size() << " not duplicate keywords" << endl << endl;
 		}
-
-		sort(v.begin(), v.end());
-
-		index_path = "./Doc/" + file_name + ".list";
-		index_file.open(index_path, ios::out);
-		if (!index_file)
-			cerr << "Error: open " << index_path << "  failed..." << endl;
-		else
-		{
-
-			for (int i = 0; i < v.size(); i++)
-			{
-				//cout << v[i] << endl;
-				index_file << v[i] << '\n';
-			}
-			doc_file.close();
-		}
-		index_file.close();
 	}
-	
-	cout << "Parse " << v.size() << " not duplicate keywords" << endl;
 
 	system("PAUSE");
 	return 0;
